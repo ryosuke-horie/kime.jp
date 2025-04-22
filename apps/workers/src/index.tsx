050afec34ce41fea7206de00916c404779ff60a0
ryosuke-horie <r_horie@sincereed.com>
2025-04-23 00:36:10 +0900

    fix: workerでHTMLレンダリングは行わないように変更しつつ、BiomeによるLintをパス

--- apps/workers/src/index.tsx
+++ apps/workers/src/index.tsx
@@ -1,12 +1,9 @@
 import { Hono } from "hono";
-import { renderer } from "./renderer";
 
 const app = new Hono();
 
-app.use(renderer);
-
 app.get("/", (c) => {
-	return c.render(<h1>Hello!</h1>);
+	return c.text("Hello!");
 });
 
 export default app;
--- apps/workers/vite.config.ts
+++ apps/workers/vite.config.ts
@@ -1,11 +1,10 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
 import build from "@hono/vite-build/cloudflare-workers";
 import { defineConfig } from "vite";
-import ssrHotReload from "vite-plugin-ssr-hot-reload";
 
 export default defineConfig(({ command, isSsrBuild }) => {
 	if (command === "serve") {
-		return { plugins: [ssrHotReload(), cloudflare()] };
+		return { plugins: [cloudflare()] };
 	}
 	if (!isSsrBuild) {
 		return {

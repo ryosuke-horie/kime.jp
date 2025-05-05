03c32562c87b9c1a75bfac79970264cd95ca66e9
ryosuke-horie <r_horie@sincereed.com>
2025-05-05 21:27:29 +0900

    chore: packages以下の不要なファイルを削除
    - 移行済みのモジュールを削除:
      - packages/api-client
      - packages/api-types
      - packages/db
      - packages/testing-utils
      - packages/validators
    
    Issue #135, #141

--- packages/api-types/tsup.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-	entry: ["src/index.ts", "src/common.ts", "src/gym.ts", "src/health.ts"],
-	format: ["esm"],
-	dts: true,
-	clean: true,
-});

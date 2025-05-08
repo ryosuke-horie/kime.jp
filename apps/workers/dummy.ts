e89220442ec8896e0d45dac8694d745039f3d4a7
ryosuke-horie <r_horie@sincereed.com>
2025-05-08 09:14:34 +0900

    refactor: 不要なダミーファイルを削除し、型チェックスクリプトを更新

--- apps/workers/dummy.ts
+++
@@ -1,6 +0,0 @@
-// Dummy file for type checking
-// This file exists just for the purpose of passing type check
-// The actual type checking will be performed in Issue #107
-
-const dummy = "dummy";
-export { dummy };
